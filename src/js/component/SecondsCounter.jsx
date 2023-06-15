0b6c69c6591bd7edbb2bd43339f60ad4c3a368eb
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-06-15 12:37:20 +0000

    cambios de diseño

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -8,13 +8,13 @@ const SecondsCounter = () => {
 
 	return (
 		<div id="main-container" className="container">
-			<div className="iconClock"> <FaRegClock /></div>
-			<div className="number">0</div>
-			<div className="number">0</div>
-			<div className="number">0</div>
-			<div className="number">0</div>
-			<div className="number">0</div>
-			<div className="number">0</div>
+			<div className="iconClock w-75 h-75"> <FaRegClock /></div>
+			<div className="number w-50 h-75">0</div>
+			<div className="number w-50 h-75">0</div>
+			<div className="number w-50 h-75">0</div>
+			<div className="number w-50 h-75">0</div>
+			<div className="number w-50 h-75">0</div>
+			<div className="number w-50 h-75">0</div>
 		</div>
 	);
 };
