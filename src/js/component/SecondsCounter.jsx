b24c56b4ef64218c1d70047824fcfee3a6433bc1
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-06-19 16:20:22 +0000

    Se añade alerta

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -5,20 +5,20 @@ import { FaRegClock } from 'react-icons/fa';
 
 
 //COMPONENTE CONTADOR DE SEGUNDOS
-const SecondsCounter = (props) => {
+const SecondsCounter = ( {seconds} ) => {
 
 	// LOGICA DE CONTADOR-------------------------------------------
 
-	let digit1 = props.seconds;
+	let digit1 = seconds;
 	let digit2 = 0;
 	let digit3 = 0;
 	let digit4 = 0;
 	let digit5 = 0;
 	let digit6 = 0;
 
-	if (props.seconds >= 9) {
-		digit2 += Math.floor(props.seconds / 10);
-		digit1 = props.seconds % 10;
+	if (seconds >= 9) {
+		digit2 += Math.floor(seconds / 10);
+		digit1 = seconds % 10;
 	} if (digit2 >= 9) {
 		digit3 += Math.floor(digit2 / 10);
 		digit2 = digit2 % 10;
@@ -31,28 +31,9 @@ const SecondsCounter = (props) => {
 	} if (digit5 >= 9) {
 		digit6 += Math.floor(digit5 / 10);
 		digit5 = digit5 % 10;
-	} else {null}
+	} 
 
-		// --------------------------------------------------------
-
-		// BOTONES DE STAR Y STOP----------------------------------
-
-		const handleStart = () => {
-
-		}
-		
-		const handleStop = () => {
-
-		}
-
-		const reverseCounter = () => {
-			let count = props.seconds;
-			setInterval(() => {
-				count -=1
-			},1000)
-		}
-
-        // --------------------------------------------------------
+	// --------------------------------------------------------
 
 
 	// DISEÑO DE CONTADOR -----------------------------------------
@@ -66,12 +47,8 @@ const SecondsCounter = (props) => {
 				<div className="number w-50 h-75">{digit3}</div>
 				<div className="number w-50 h-75">{digit2}</div>
 				<div className="number w-50 h-75">{digit1}</div>
-				<div>
-					<button type="text" onClick={reverseCounter}>Reverse</button>
-				</div>
 			</div>
-			
-			
+				
 		);
 	};
 
--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,19 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 import SecondsCounter from "./component/SecondsCounter.jsx";
 
-
-
 let counter = 0
+
+const counterAlert = () => {
+    if(counter === 369) {
+        alert("Se alcanzó el tiempo máximo")
+    }
+}
 
 setInterval(() => {
     ReactDOM.render(<SecondsCounter seconds={counter} />, document.querySelector("#app"));
     counter += 1;
-}, 1000)
+    counterAlert();
+}, 1000);
 
 
 
